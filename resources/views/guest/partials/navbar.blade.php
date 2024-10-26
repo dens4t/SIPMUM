8b57783de5091347e9856a26d80d956ee7d570df
LAPTOP-A5R3RD4O\Acer <ridhoananda390@gmail.com>
2024-10-27 06:55:37 +0700

    fix navbar

--- resources/views/guest/partials/navbar.blade.php
+++ resources/views/guest/partials/navbar.blade.php
@@ -5,17 +5,18 @@
             <a href=""><div class="ms ms-yt"></div></a>
          </div>
         <section class="top-h" style="background-color: #14a2ba !important;"> 
-            <div class="row justify-content-center">
-                <div class="col-lg-4 d-flex justify-content-between text-white" style="font-family:Arial, Helvetica, sans-serif; font-size: 12px;">
-                    <p><i class="bi bi-telephone-fill"></i>  08080808080</p> 
-                    <p><i class="bi bi-envelope-fill"></i> lorem@gmail.com</p>
-                    <p><i class="bi bi-geo-alt-fill"></i>  Jl Ayani bla bla bla</p> 
+            <div class="row justify-content-center p-2">
+                <div class="col-lg-4 d-flex justify-content-between align-items-center text-white" style="font-family:Arial, Helvetica, sans-serif; font-size: 12px;">
+                    <span><i class="bi bi-telephone-fill"></i>  08xxxxxxxx</span> 
+                    <span><i class="bi bi-envelope-fill"></i> lorem@gmail.com</span>
+                    <span><i class="bi bi-geo-alt-fill"></i>  Jl Lorem, ipsum dolor.</span> 
                 </div> 
                 <div class="col-lg-3 d-flex justify-content-end align-items-center text-white" style="font-family:Arial, Helvetica, sans-serif; font-size: 10px;">
                     <form class="d-flex" role="search">
                       <input class="me-2" type="search" placeholder="Search" aria-label="Search">
-                      <button class="" type="submit">Search</button>
+                      <button class="myBtn" type="submit"><i class="bi bi-search"></i></button>
                     </form>
+                    <a class="nav-link ms-3 myBtna" href="{{ url('admin') }}">login</a>
                 </div>
             </div>
         </section>
@@ -29,35 +30,33 @@
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                               Profil
                             </a>
-                            <ul class="dropdown-menu" style="border: 0 !important; border-radius: 0; font-family:Arial, Helvetica, sans-serif;">
-                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('tentang-kami') }}">Tentang Kami</a></li>
-                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('struktur-organisasi') }}">Struktur Organisasi</a></li>
-                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('penghargaan') }}">Penghargaan</a></li>
-                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('budaya-perusahaan') }}">Budaya Perusahaan</a></li>
-                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('sertifikasi') }}">Sertifikasi</a></li>
+                            <ul class="dropdown-menu animate slideIn" style="border: 0 !important; border-radius: 0; font-family:Arial, Helvetica, sans-serif;" data-aos="fade-down">
+                            <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('tentang-kami') }}">Tentang Kami</a></li>
+                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('struktur-organisasi') }}">Struktur Organisasi</a></li>
+                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('penghargaan') }}">Penghargaan</a></li>
+                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('budaya-perusahaan') }}">Budaya Perusahaan</a></li>
+                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('sertifikasi') }}">Sertifikasi</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                               Unit Pembangkit
                             </a>
-                            <ul class="dropdown-menu " style="border: 0 !important; border-radius: 0; font-family:Arial, Helvetica, sans-serif;">
-                            <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">Kantor UP Kapuas </a></li>
-                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">ULPLTD/G Siantan</a></li>
-                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">UPLTD SEI RAYA</a></li>
-                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">UPLTD SANGGAU</a></li>
-                        <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">UPTLD KETAPANG</a></li>
+                            <ul class="dropdown-menu animate slideIn" style="border: 0 !important; border-radius: 0; font-family:Arial, Helvetica, sans-serif;">
+                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">ULPLTD/G Siantan</a></li>
+                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">UPLTD SEI RAYA</a></li>
+                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">UPLTD SANGGAU</a></li>
+                                <li><a class="dropdown-item fw-bolder text-muted" href="{{ url('unit') }}">UPTLD KETAPANG</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                               Media
                             </a>
-                            <ul class="dropdown-menu"  style="border: 0 !important; border-radius: 0; font-family:Arial, Helvetica, sans-serif;">
+                            <ul class="dropdown-menu animate slideIn"  style="border: 0 !important; border-radius: 0; font-family:Arial, Helvetica, sans-serif;">
                               <li><a class="dropdown-item fw-bolder text-muted" href="#"><i class="bi bi-collection-play-fill"></i> Siaran Pers</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('admin') }}">login</a></li>
                     </ul>
                 </div>
                 <a class="navbar-brand" href="#page-top"><img src="./assets/img/Logo-PLN.png" alt="PLN Nusantara" class="lytzd-logo"></a>
